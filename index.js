0b9ccf8d2a25b4af434d3eb7c2a32c86b0c728ea
elsive2 <66211474+elsive2@users.noreply.github.com>
2022-05-12 18:40:09 +0600

    Prepare handlebars and .hbs files

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express')
+
 const app = express()
+
+const hbs = require('express-handlebars').create({
+	defaultLayout: 'main',
+	extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+
+app.get('/', (request, response) => {
+	response.render('index')
+})
 
 const PORT = process.env.PORT || 3000
 
