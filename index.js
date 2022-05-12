7460b0c3cbf4f753d326ac344ef3148720d25e57
elsive2 <66211474+elsive2@users.noreply.github.com>
2022-05-12 19:14:49 +0600

    Add missing routes create dynamic title

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const app = express()
 
 const hbs = require('express-handlebars').create({
@@ -12,8 +11,26 @@ app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 
+// routes
 app.get('/', (request, response) => {
-	response.render('index')
+	response.render('index', {
+		title: 'Home page',
+		isHomePage: true
+	})
+})
+
+app.get('/courses', (request, response) => {
+	response.render('courses', {
+		title: 'Courses page',
+		isCoursesPage: true
+	})
+})
+
+app.get('/create', (request, response) => {
+	response.render('create', {
+		title: 'Create a new course',
+		isCreatePage: true
+	})
 })
 
 const PORT = process.env.PORT || 3000
