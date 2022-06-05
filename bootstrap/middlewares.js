a1849b5dfd201014add6964bc95657abc787adc3
elsive2 <66211474+elsive2@users.noreply.github.com>
2022-06-05 13:55:19 +0600

    Compress response bodies for all requests

--- bootstrap/middlewares.js
+++ bootstrap/middlewares.js
@@ -4,6 +4,7 @@ const variablesMiddleware = require('../middlewares/variables')
 const userMiddleware = require('../middlewares/user')
 const fileMiddleware = require('../middlewares/file')
 const helmet = require('helmet')
+const compression = require('compression')
 
 module.exports = function (app) {
 	app.use(csruf())
@@ -12,4 +13,5 @@ module.exports = function (app) {
 	app.use(userMiddleware)
 	app.use(fileMiddleware.single('avatar'))
 	app.use(helmet())
+	app.use(compression())
 }
